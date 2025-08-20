07e3510e4aff00da7a18f786eea8cfef50881a4f
Danhulck <familiaevd@yahoo.com.br>
2025-08-20 11:59:20 -0300

    aula

---
+++ Projeto/js/compare.js
@@ -0,0 +1,79 @@
+
+//car
+let carArr = [];
+
+class Car {
+   
+
+    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
+
+        this.nome = nome; 
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        this.image = image;
+
+
+    }
+} 
+
+// search on array if exist carClass returning 1 if not return -1
+function GetCarArrPosition(arr, carClass) {
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].nome  === carClass.nome)
+            return i;
+    }
+    return -1;
+}
+
+function SetCarToCompare(el, carClass) {
+   
+    if(carClass instanceof Car){       
+        if(el.checked){
+            if(carArr.length){
+                el.checked = false;
+                alert("Você só pode comprar 2 veículos por vez!")
+                return;
+            }
+            if(GetCarArrPosition(carArr, carClass) === -1){
+                carArr.push(carClass);
+            }
+        } else {
+            const index = GetCarArrPosition(carArr, carClass);
+            if(index !== 1){
+                carArr.splice(index, 1);
+            }
+        } 
+    } else {
+        throw "You need set a Car Class";
+    }
+    
+}
+
+function botaoComparar(){
+    const compareBtn =  document.querySelector("button[onclick=1'ShowCompare]'");
+}
+
+function ShowCompare() {
+    if(carArr.length < 2) {
+        alert("Precisa marcar 2 carros para apresentar a comparação");
+        return;
+    }
+
+    UpdateCompareTable();
+    document.getElementById("compare").style.display = "block";
+}
+
+function HideCompare(){
+    document.getElementById("compare").style.display = "none"; 
+}
+
+function UpdateCompareTable() {
+    
+}
